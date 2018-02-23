2c2725c80db72c37106225f7fdf89b17f7686346
Luis Enrique Ramos <lenrique41@hotmail.com>
2018-02-23 00:32:30 -0600

    Instalado dependencias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,31 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { environment } from '../environments/environment';
+import  { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { TasksComponent } from './components/tasks/tasks.component';
+
+import { TaskService } from './services/task.service';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    AddTaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase, 'angularfs'),
+    AngularFirestoreModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    TaskService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyDp3Mnbnwf-_fan0OyrbVPxwvXGbx2hgkw",
+    authDomain: "angular-fs-31942.firebaseapp.com",
+    databaseURL: "https://angular-fs-31942.firebaseio.com",
+    projectId: "angular-fs-31942",
+    storageBucket: "angular-fs-31942.appspot.com",
+    messagingSenderId: "459346653147"
+  }
 };
